f4cf6b02a22f86a27d6358530a2fb35de065f3fa
Sara Najafi <sara.njf.ch@gmail.com>
2023-08-16 04:00:38 +0330

    database
    login

--- src/service/user.login.ts
+++ src/service/user.login.ts
@@ -1,16 +1,22 @@
+import { AppDataSource } from "../data-source";
 import { userRepository } from "../dependency";
 import { HttpError } from "../errors/myerrors";
+import { UserEntity } from "../repository/userRepository/entity/user.entity";
 import { User, UserRepository } from "../repository/userRepository/user.repository";
 import { UserDto } from "./user.dto";
 
 const userRepo = new UserRepository();
-export const login= (user: UserDto)=>{
-    const loggedInUser = userRepo.findUserByUsername(user.username);
+//const userRepo = AppDataSource.getRepository(UserEntity);
+export const login= async (user: UserDto)=>{
+    const loggedInUser =await userRepo.findUserByUsername(user.username);
     if(loggedInUser === null ){
         throw new HttpError(401,"Invalid username or password");
     }
-    if(loggedInUser.password !== user.password){ /// chera???
+    if(loggedInUser.password === user.password){
         return loggedInUser;
     }
     
 };
+
+
+// inja moshkel daram
